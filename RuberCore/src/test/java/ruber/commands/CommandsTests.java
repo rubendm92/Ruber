6e05ccc009c7d14b5aeca61e0211be945119be6e
Rubén Díaz Martínez <rubendm92@gmail.com>
2014-08-10 20:18:20 +0200

    Modify test for SignTeachingsCommand

--- RuberCore/src/test/java/ruber/commands/CommandsTests.java
+++ RuberCore/src/test/java/ruber/commands/CommandsTests.java
@@ -2,13 +2,15 @@ package ruber.commands;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
+import ruber.model.TeachingList;
 import ruber.model.fake.Professors;
 import ruber.model.fake.Teachings;
 import ruber.persistence.SignedTeachingsSaver;
 import ruber.view.Command;
 import ruber.viewmodels.FrameViewModel;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.*;
 
 public class CommandsTests {
@@ -23,15 +25,19 @@ public class CommandsTests {
     @Test
     public void executingCloseSessionCommandShouldCallToClearFrame() {
         Command command = new CloseSessionCommand(frame);
+
         command.execute();
+
         verify(frame).clear();
     }
 
     @Test
     public void executingStartSessionCommandShouldShowProfessorAndTeachingsForHim() {
+        Command command = new StartSessionCommand(frame, Professors.list(), Teachings.longList());
+
         when(frame.getDni()).thenReturn("44739382");
-        Command command = new StartSessionCommand(frame, Professors.list(),Teachings.longList());
         command.execute();
+
         verify(frame).initSession(Professors.ruben());
         verify(frame).showTeachings(Teachings.fsoList());
     }
@@ -39,15 +45,19 @@ public class CommandsTests {
     @Test
     public void executingShowProfessorsCommandShouldShowAllProfessorsWithTeachingNow() {
         Command command = new ShowProfessorsCommand(frame, Teachings.nowList());
+
         command.execute();
+
         verify(frame).showProfessors(Professors.rubenList());
     }
 
     @Test
     public void executingShowTeachingsToReplaceCommandShouldShowAllTeachingsUnsignedForAProfessor() {
-        when(frame.getProfessorToReplace()).thenReturn(Professors.replacement());
         Command command = new ShowTeachingsToReplaceCommand(frame, Teachings.longList());
+
+        when(frame.getProfessorToReplace()).thenReturn(Professors.replacement());
         command.execute();
+
         verify(frame).showProfessorToReplace();
         verify(frame).showTeachings(Teachings.is2List());
     }
@@ -55,13 +65,15 @@ public class CommandsTests {
     @Test
     public void executingSignTeachingsCommandShouldSignTeachingsSelected() {
         final SignedTeachingsSaver saver = mock(SignedTeachingsSaver.class);
-        when(frame.getSelectedTeachings()).thenReturn(Teachings.longList());
-        Command command = new SignTeachingsCommand(frame, saver);
+        final TeachingList teachings = Teachings.fsoList();
+        final Command command = new SignTeachingsCommand(frame, saver);
+
+        when(frame.getProfessorFromSession()).thenReturn(Professors.ruben());
+        when(frame.getProfessorSelected()).thenReturn(Professors.ruben());
+        when(frame.getSelectedTeachings()).thenReturn(teachings);
         command.execute();
-        verify(frame, atLeast(1)).getSelectedTeachings();
-        verify(frame, atLeast(1)).getProfessorSelected();
-        verify(frame, atLeast(1)).getProfessorFromSession();
-        verify(frame, atLeast(1)).getSignature();
-        verify(saver, atLeast(1)).save(Mockito.any(), Mockito.any());
+
+        verify(saver).save(Professors.ruben(), teachings);
+        teachings.forEach(teaching -> assertThat(teaching.getProfessorForWhomSigned(Professors.ruben()), is(Professors.ruben())));
     }
 }
